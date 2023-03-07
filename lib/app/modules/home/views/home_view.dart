4bad9f111a878fffba12f26bcc682ba8611c9c1a
khanh.tran <khanh.tran@enlabsoftware.com>
2023-03-07 21:20:06 +0700

    Add SystemUiOverlayStyle.light for ios

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 import 'package:quiz_demo/app/constant.dart';
@@ -19,38 +20,41 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Spacer(flex: 2), //2/6
-              Text(
-                "Let's Play Quiz,",
-                style: Theme.of(context).textTheme.headline4?.copyWith(
-                    color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              const Text("Enter your informations below"),
-              const Spacer(), // 1/6
-              TextField(
-                decoration: const InputDecoration(
-                  filled: true,
-                  hintText: "Full Name",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                  ),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Spacer(flex: 2), //2/6
+                Text(
+                  "Let's Play Quiz,",
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-                onChanged: _onNameChanged,
-              ),
-              const Spacer(), // 1/6
-              AppButton(
-                width: Get.width,
-                onPressed: _onPressed,
-                labelText: "Start Quiz",
-              ),
-              const Spacer(flex: 2), // it will take 2/6 spaces
-            ],
+                const Text("Enter your informations below"),
+                const Spacer(), // 1/6
+                TextField(
+                  decoration: const InputDecoration(
+                    filled: true,
+                    hintText: "Full Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                  onChanged: _onNameChanged,
+                ),
+                const Spacer(), // 1/6
+                AppButton(
+                  width: Get.width,
+                  onPressed: _onPressed,
+                  labelText: "Start Quiz",
+                ),
+                const Spacer(flex: 2), // it will take 2/6 spaces
+              ],
+            ),
           ),
         ),
       ),
